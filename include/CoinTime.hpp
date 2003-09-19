69e524d42f51fe80e2405a6c40a034b0a17301f3
(no author) <anonymous@coin-or.clemson.edu>
2003-09-19 21:01:37 +0000

    This commit was manufactured by cvs2svn to create branch 'pre'.

---
+++ include/CoinTime.hpp
@@ -0,0 +1,39 @@
+// Copyright (C) 2002, International Business Machines
+// Corporation and others.  All Rights Reserved.
+
+#ifndef _CoinTime_hpp
+#define _CoinTime_hpp
+
+//#############################################################################
+
+#include <ctime>
+#if defined(_MSC_VER)
+// Turn off compiler warning about long names
+#  pragma warning(disable:4786)
+#else
+#include <sys/resource.h>
+#endif
+
+//#############################################################################
+
+static inline double CoinCpuTime()
+{
+  double cpu_temp;
+#if defined(_MSC_VER)
+  unsigned int ticksnow;        /* clock_t is same as int */
+  
+  ticksnow = (unsigned int)clock();
+  
+  cpu_temp = (double)((double)ticksnow/CLOCKS_PER_SEC);
+#else
+  struct rusage usage;
+  getrusage(RUSAGE_SELF,&usage);
+  cpu_temp = usage.ru_utime.tv_sec;
+  cpu_temp += 1.0e-6*((double) usage.ru_utime.tv_usec);
+#endif
+  return cpu_temp;
+}
+
+//#############################################################################
+
+#endif
