ebf27178a3dd97c0d08df001a63784a41e575507
Laci Ladanyi <ladanyi@us.ibm.com>
2003-11-14 01:21:28 +0000

    added a CoinTimer class with retracing capabilities

--- include/CoinTime.hpp
+++ include/CoinTime.hpp
@@ -40,4 +40,71 @@ static inline double CoinCpuTime()
 
 //#############################################################################
 
+class CoinTimer
+{
+private:
+   inline bool evaluate(const bool past) {
+#ifndef COIN_COMPILE_WITH_TRACING
+      int ipast = past;
+      if (stream) {
+	 
+	 if (write_stream)
+	    stream << ipast << "\n";
+	 else 
+	    stream >> ipast;
+      }
+      return ipast;
+#else
+      return past;
+#endif
+   }
+
+private:
+   double start;
+   double limit;
+   double end;
+#ifdef COIN_COMPILE_WITH_TRACING
+   std::fstream* stream;
+   bool write_stream;
+#endif
+
+public:
+   CoinTimer() :
+      start(0), limit(DBL_MAX), end(DBL_MAX)
+#ifdef COIN_COMPILE_WITH_TRACING
+      , stream(0), write_stream(true)
+#endif
+   {}
+   CoinTimer(double lim) :
+      start(CoinCpuTime()), limit(lim), end(start+lim)
+#ifdef COIN_COMPILE_WITH_TRACING
+      , stream(0), write_stream(true)
+#endif
+   {}
+
+#ifdef COIN_COMPILE_WITH_TRACING
+   CoinTimer(std::fstream* s, bool w) :
+      start(0), limit(DBL_MAX), end(DBL_MAX),
+      stream(s), write_stream(true) {}
+   CoinTimer(double lim, std::fstream* s, bool w) :
+      start(CoinCpuTime()), limit(lim), end(start+lim),
+      stream(s), write_stream(true) {}
+#endif
+   
+
+   inline void restart() { start=CoinCpuTime(); end=start+limit; }
+   inline void reset() { restart(); }
+   inline void reset(double lim) { limit=lim; restart(); }
+
+   inline bool isPastPercent(double pct) {
+      return evaluate(start + limit * pct > CoinCpuTime());
+   }
+   inline bool isPast(double lim) {
+      return evaluate(start + lim > CoinCpuTime());
+   }
+   inline bool isExpired() {
+      return evaluate(end < CoinCpuTime());
+   }
+}
+
 #endif
