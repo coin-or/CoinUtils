2679d0ab4a6d02d6fd622c1240b86c60ccb837be
Lou Hafer <lou@sfu.ca>
2004-09-16 16:26:09 +0000

    Documentation; minor cleanup.

--- include/CoinPresolveDupcol.hpp
+++ include/CoinPresolveDupcol.hpp
@@ -3,7 +3,27 @@
 
 #ifndef CoinPresolveDupcol_H
 #define CoinPresolveDupcol_H
+
+/*!
+  \file
+*/
+
 #define	DUPCOL	10
+
+/*! \class dupcol_action
+    \brief Detect and remove duplicate columns
+
+    The general technique is to sum the coefficients a_(*,j) of each column.
+    Columns with identical sums are duplicates. The obvious problem is that,
+    <i>e.g.</i>, [1 0 1 0] and [0 1 0 1] both add to 2. To minimize the
+    chances of false positives, the coefficients of each row are multipled by
+    a random number r_i, so that we sum r_i*a_ij.
+
+   Candidate columns are checked to confirm they are identical. Where the
+   columns have the same objective coefficient, the two are combined. If the
+   columns have different objective coefficients, complications ensue. In order
+   to remove the duplicate, it must be possible to fix the variable at a bound.
+*/
 
 class dupcol_action : public CoinPresolveAction {
   struct action {
@@ -14,7 +34,6 @@ class dupcol_action : public CoinPresolveAction {
     int ithis;
     int ilast;
 
-    int *colrows;
     double *colels;
     int nincol;
   };
@@ -23,11 +42,8 @@ class dupcol_action : public CoinPresolveAction {
   const action *const actions_;
 
   dupcol_action():CoinPresolveAction(NULL),nactions_(0),actions_(NULL) {};
-  dupcol_action(int nactions,
-		const action *actions,
-		const CoinPresolveAction *next)/* :
-    nactions_(nactions), actions_(actions),
-    CoinPresolveAction(next) {}*/;
+  dupcol_action(int nactions, const action *actions,
+		const CoinPresolveAction *next) ;
 
  public:
   const char *name() const;
@@ -37,9 +53,22 @@ class dupcol_action : public CoinPresolveAction {
 
   void postsolve(CoinPostsolveMatrix *prob) const;
 
-  //~dupcol_action() { delete[]actions_; }
 };
 
+
+/*! \class duprow_action
+    \brief Detect and remove duplicate rows
+
+    The algorithm to detect duplicate rows is as outlined for dupcol_action.
+
+    If the feasible interval for one constraint is strictly contained in the
+    other, the tighter (contained) constraint is kept. If the feasible
+    intervals are disjoint, the problem is infeasible. If the feasible
+    intervals overlap, both constraints are kept.
+
+    duprow_action is definitely a work in progress; #postsolve is
+    unimplemented.
+*/
 
 class duprow_action : public CoinPresolveAction {
   struct action {
--- include/CoinPresolveForcing.hpp
+++ include/CoinPresolveForcing.hpp
@@ -3,8 +3,23 @@
 
 #ifndef CoinPresolveForcing_H
 #define CoinPresolveForcing_H
+
+/*!
+  \file
+*/
+
 #define	IMPLIED_BOUND	7
 
+/*! \class forcing_constraint_action
+    \brief Detect and process forcing constraints and useless constraints
+
+  A constraint is useless if the bounds on the variables prevent the constraint
+  from ever being violated.
+
+  A constraint is a forcing constraint if the bounds on the constraint force
+  the value of an involved variable to one of its bounds. A constraint can
+  force more than one variable.
+*/
 class forcing_constraint_action : public CoinPresolveAction {
   struct action {
     const int *rowcols;
@@ -33,12 +48,5 @@ class forcing_constraint_action : public CoinPresolveAction {
 
   ~forcing_constraint_action();
 };
-
-
-
-
-// HACK - from doubleton.cpp
-void compact_rep(double *elems, int *indices, CoinBigIndex *starts, const int *lengths, int n,
-		 const presolvehlink *link);
 
 #endif
