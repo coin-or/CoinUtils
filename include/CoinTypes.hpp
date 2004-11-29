b63d144971eb3ae9451472d3e756cd4f7f1eea00
Laci Ladanyi <ladanyi@us.ibm.com>
2004-11-29 21:28:05 +0000

    Added a new header to declare various platform dependent types

---
+++ include/CoinTypes.hpp
@@ -0,0 +1,17 @@
+// Copyright (C) 2004, International Business Machines
+// Corporation and others.  All Rights Reserved.
+
+#ifndef _CoinTypes_hpp
+#define _CoinTypes_hpp
+
+//#############################################################################
+
+#if defined(_MSC_VER)
+   typedef __int64 int64_t;
+#else
+#  include <climits>
+#endif
+
+//#############################################################################
+
+#endif
