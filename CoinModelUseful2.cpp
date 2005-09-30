01f147c54ba48cd8609143e3073936a92013f9b0
John Forrest <john.forrest@fastercoin.com>
2005-09-30 18:58:42 +0000

    license

--- CoinModelUseful2.cpp
+++ CoinModelUseful2.cpp
@@ -2,6 +2,8 @@
 // Corporation and others.  All Rights Reserved.
 /* A Bison parser, made by GNU Bison 1.875c.  */
 
+// License sounds scary but see special exception so has no problems
+
 /* Skeleton parser for Yacc-like parsing with Bison,
    Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003 Free Software Foundation, Inc.
 
