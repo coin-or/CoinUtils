0838b3138d3808af622c4f668cb99d4d9aa6325d
JP Fasano <jpfasano@gmail.com>
2006-05-19 00:44:10 +0000

    modifed include syntax to work on windows ms v6

--- CoinUtils/src/CoinSort.hpp
+++ CoinUtils/src/CoinSort.hpp
@@ -5,7 +5,7 @@
 
 #include <functional>
 #include <new>
-#include <CoinDistance.hpp>
+#include "CoinDistance.hpp"
 #include "CoinFinite.hpp"
 
 // Uncomment the next three lines to get thorough initialisation of memory.
