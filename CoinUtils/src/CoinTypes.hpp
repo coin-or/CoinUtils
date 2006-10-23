c834fee244903ac0a429810b710f969597a9a6c1
Andreas Waechter <andreas.waechter@northwestern.edu>
2006-10-23 22:49:28 +0000

    added test for integer types (64bit and pointer)

--- CoinUtils/src/CoinTypes.hpp
+++ CoinUtils/src/CoinTypes.hpp
@@ -4,6 +4,19 @@
 #ifndef _CoinTypes_hpp
 #define _CoinTypes_hpp
 
+#include "CoinUtilsConfig.h"
+
+#ifdef HAVE_CINTTYPES
+# include <cinttypes>
+#else
+# ifdef HAVE_INTTYPES_H
+#  include <inttypes.h>
+# endif
+#endif
+
+#define CoinInt64 COIN_INT64_T
+#define CoinIntPtr COIN_INTPTR_T
+
 //#############################################################################
 
 #if defined(_MSC_VER)
