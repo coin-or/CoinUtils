ede86311c5562a10af98a8b4c8dad1e45fb6b965
Laci Ladanyi <ladanyi@us.ibm.com>
2006-11-12 02:30:22 +0000

    The candidate list in a coin search tree now contains sibling lists instead of single nodes

--- CoinUtils/src/CoinSearchTree.hpp
+++ CoinUtils/src/CoinSearchTree.hpp
@@ -6,6 +6,7 @@
 #include <cmath>
 
 #include "CoinFinite.hpp"
+#include "CoinHelperFunctions.hpp"
 
 //#############################################################################
 
@@ -44,6 +45,35 @@ public:
     // Default (bitwise) constr, copy constr, assignment op, and destr all OK.
 };
 
+class CoinTreeSiblings {
+private:
+    CoinTreeSiblings();
+    CoinTreeSiblings& operator=(const CoinTreeSiblings&);
+private:
+    int current_;
+    int numSiblings_;
+    CoinTreeNode** siblings_;
+public:
+    CoinTreeSiblings(const int n, CoinTreeNode** nodes) :
+	current_(0), numSiblings_(n), siblings_(new CoinTreeNode*[n])
+    {
+	CoinDisjointCopyN(nodes, n, siblings_);
+    }
+    CoinTreeSiblings(const CoinTreeSiblings& s) :
+	current_(s.current_),
+	numSiblings_(s.numSiblings_),
+	siblings_(new CoinTreeNode*[s.numSiblings_])
+    {
+	CoinDisjointCopyN(s.siblings_, s.numSiblings_, siblings_);
+    }
+    ~CoinTreeSiblings() { delete[] siblings_; }
+    inline CoinTreeNode* currentNode() const { return siblings_[current_]; }
+    /** returns false if cannot be advanced */
+    inline bool advanceNode() { return ++current_ != numSiblings_; }
+    inline int toProcess() const { return numSiblings_ - current_; }
+    inline int size() const { return numSiblings_; }
+};
+
 //#############################################################################
 
 /** Function objects to compare search tree nodes. The comparison function
@@ -52,27 +82,27 @@ public:
 /*@{*/
 /** Depth First Search. */
 struct CoinSearchTreeCompareDepth {
-    inline bool operator()(const CoinTreeNode* x,
-			   const CoinTreeNode* y) const {
-	return x->depth_ > y->depth_;
+    inline bool operator()(const CoinTreeSiblings* x,
+			   const CoinTreeSiblings* y) const {
+	return x->currentNode()->depth_ > y->currentNode()->depth_;
     }
 };
 
 //-----------------------------------------------------------------------------
 /* Breadth First Search */
 struct CoinSearchTreeCompareBreadth {
-    inline bool operator()(const CoinTreeNode* x,
-			   const CoinTreeNode* y) const {
-	return x->depth_ < y->depth_;
+    inline bool operator()(const CoinTreeSiblings* x,
+			   const CoinTreeSiblings* y) const {
+	return x->currentNode()->depth_ < y->currentNode()->depth_;
     }
 };
 
 //-----------------------------------------------------------------------------
 /** Best first search */
 struct CoinSearchTreeCompareBest {
-    inline bool operator()(const CoinTreeNode* x,
-			   const CoinTreeNode* y) const {
-	return x->quality_ < y->quality_;
+    inline bool operator()(const CoinTreeSiblings* x,
+			   const CoinTreeSiblings* y) const {
+	return x->currentNode()->quality_ < y->currentNode()->quality_;
     }
 };
 
@@ -85,24 +115,61 @@ private:
     CoinSearchTreeBase& operator=(const CoinSearchTreeBase&);
 
 protected:
-    std::vector<CoinTreeNode*> nodes_;
-    size_t numInserted_;
+    std::vector<CoinTreeSiblings*> candidateList_;
+    int numInserted_;
+    int size_;
 
 protected:
-    CoinSearchTreeBase() : nodes_(), numInserted_(0) {}
+    CoinSearchTreeBase() : candidateList_(), numInserted_(0), size_(0) {}
+
+    virtual void realpop() = 0;
+    virtual void realpush(CoinTreeSiblings* s) = 0;
+    virtual void fixTop() = 0;
 
 public:
     virtual ~CoinSearchTreeBase() {}
 
-    inline const std::vector<CoinTreeNode*>& getNodes() const {
-	return nodes_;
+    inline const std::vector<CoinTreeSiblings*>& getCandidates() const {
+	return candidateList_;
     }
-    inline bool empty() const { return nodes_.empty(); }
-    inline size_t size() const { return nodes_.size(); }
-    inline size_t numInserted() const { return numInserted_; }
-    inline CoinTreeNode* top() const { return nodes_.front(); }
-    virtual void pop() = 0;
-    virtual void push(CoinTreeNode* node, const bool incrInserted = true) = 0;
+    inline bool empty() const { return candidateList_.empty(); }
+    inline int size() const { return size_; }
+    inline int numInserted() const { return numInserted_; }
+    inline CoinTreeNode* top() const {
+	CoinTreeSiblings* s = candidateList_.front();
+	return s ? s->currentNode() : NULL;
+    }
+    /** pop will advance the \c next pointer among the siblings on the top and
+	then moves the top to its correct position. #realpop is the method
+	that actually removes the element from the heap */
+    inline void pop() {
+	CoinTreeSiblings* s = candidateList_.front();
+	if (!s->advanceNode()) {
+	    realpop();
+	    delete s;
+	} else {
+	    fixTop();
+	}
+	--size_;
+    }
+    inline void push(int numNodes, CoinTreeNode** nodes,
+		     const bool incrInserted = true) {
+	CoinTreeSiblings* s = new CoinTreeSiblings(numNodes, nodes);
+	realpush(s);
+	if (incrInserted) {
+	    numInserted_ += numNodes;
+	}
+	size_ += numNodes;
+    }
+    inline void push(const CoinTreeSiblings& sib,
+		     const bool incrInserted = true) {
+	CoinTreeSiblings* s = new CoinTreeSiblings(sib);
+	realpush(s);
+	if (incrInserted) {
+	    numInserted_ += sib.toProcess();
+	}
+	size_ += sib.size();
+    }
 };
 
 //#############################################################################
@@ -114,27 +181,29 @@ class CoinSearchTree : public CoinSearchTreeBase
 {
 private:
     Comp comp_;
+protected:
+    virtual void realpop() {
+	candidateList_.pop();
+    }
+    virtual void fixTop() {
+	CoinTreeSiblings* s = top();
+	realpop();
+	push(s, false);
+    }
+    virtual void realpush(CoinTreeSiblings* s) {
+	nodes_.push_back(s);
+	std::push_heap(candidateList_.begin(), candidateList_.end(), comp_);
+    }
 public:
     CoinSearchTree() : CoinSearchTreeBase(), comp_() {}
     CoinSearchTree(const CoinSearchTreeBase& t) :
 	CoinSearchTreeBase(), comp_() {
-	nodes_ = t.getNodes();
-	std::make_heap(nodes_.begin(), nodes_.end(), comp_);
-	numInserted_ = nodes_.size();
+	candidateList_ = t.getCandidates();
+	std::make_heap(candidateList_.begin(), candidateList_.end(), comp_);
+	numInserted_ = t.numInserted_;
+	size_ = t.size_;
     }
     ~CoinSearchTree() {}
-
-    void pop() {
-	std::pop_heap(nodes_.begin(), nodes_.end(), comp_);
-	nodes_.pop_back();
-    }
-    void push(CoinTreeNode* node, const bool incrInserted = true) {
-	nodes_.push_back(node);
-	std::push_heap(nodes_.begin(), nodes_.end(), comp_);
-	if (incrInserted) {
-	    ++numInserted_;
-	}
-    }
 };
 
 #else
@@ -144,57 +213,62 @@ class CoinSearchTree : public CoinSearchTreeBase
 {
 private:
     Comp comp_;
+
+protected:
+    virtual void realpop() {
+	candidateList_[0] = candidateList_.back();
+	candidateList_.pop_back();
+	fixTop();
+    }
+    /** After changing data in the top node, fix the heap */
+    virtual void fixTop() {
+	const int size = candidateList_.size();
+	if (size > 1) {
+	    CoinTreeSiblings** candidates = &candidateList_[0];
+	    CoinTreeSiblings* s = candidates[0];
+	    --candidates;
+	    int pos = 1;
+	    int ch;
+	    for (ch = 2; ch < size; pos = ch, ch *= 2) {
+		if (comp_(candidates[ch+1], candidates[ch]))
+		    ++ch;
+		if (comp_(s, candidates[ch]))
+		    break;
+		candidates[pos] = candidates[ch];
+	    }
+	    if (ch == size) {
+		if (comp_(candidates[ch], s)) {
+		    candidates[pos] = candidates[ch];
+		    pos = ch;
+		}
+	    }
+	    candidates[pos] = s;
+	}
+    }
+    virtual void realpush(CoinTreeSiblings* s) {
+	candidateList_.push_back(s);
+	CoinTreeSiblings** candidates = &candidateList_[0];
+	--candidates;
+	int pos = candidateList_.size();
+	int ch;
+	for (ch = pos/2; ch != 0; pos = ch, ch /= 2) {
+	    if (comp_(candidates[ch], s))
+		break;
+	    candidates[pos] = candidates[ch];
+	}
+	candidates[pos] = s;
+    }
+  
 public:
     CoinSearchTree() : CoinSearchTreeBase(), comp_() {}
     CoinSearchTree(const CoinSearchTreeBase& t) :
 	CoinSearchTreeBase(), comp_() {
-	nodes_ = t.getNodes();
-	std::sort(nodes_.begin(), nodes_.end(), comp_);
-	numInserted_ = nodes_.size();
+	candidateList_ = t.getCandidates();
+	std::sort(candidateList_.begin(), candidateList_.end(), comp_);
+	numInserted_ = t.numInserted();
+	size_ = t.size();
     }
     ~CoinSearchTree() {}
-
-    void pop() {
-	CoinTreeNode* node = nodes_.back();
-	nodes_.pop_back();
-	const int size = nodes_.size();
-	if (size > 0) {
-	    CoinTreeNode** nodes = &nodes_[0];
-	    --nodes;
-	    int pos = 1;
-	    int ch;
-	    for (ch = 2; ch < size; pos = ch, ch *= 2) {
-		if (comp_(nodes[ch+1], nodes[ch]))
-		    ++ch;
-		if (comp_(node, nodes[ch]))
-		    break;
-		nodes[pos] = nodes[ch];
-	    }
-	    if (ch == size) {
-		if (comp_(nodes[ch], node)) {
-		    nodes[pos] = nodes[ch];
-		    pos = ch;
-		}
-	    }
-	    nodes[pos] = node;
-	}
-    }
-    void push(CoinTreeNode* node, const bool incrInserted = true) {
-	nodes_.push_back(node);
-	CoinTreeNode** nodes = &nodes_[0];
-	--nodes;
-	int pos = nodes_.size();
-	int ch;
-	for (ch = pos/2; ch != 0; pos = ch, ch /= 2) {
-	    if (comp_(nodes[ch], node))
-		break;
-	    nodes[pos] = nodes[ch];
-	}
-	nodes[pos] = node;
-	if (incrInserted) {
-	    ++numInserted_;
-	}
-    }
 };
 
 #endif
@@ -246,7 +320,14 @@ public:
     inline CoinTreeNode* top() const { return candidates_->top(); }
     inline void pop() { candidates_->pop(); }
     inline void push(CoinTreeNode* node, const bool incrInserted = true) {
-	candidates_->push(node, incrInserted);
+	candidates_->push(1, &node, incrInserted);
+    }
+    inline void push(const CoinTreeSiblings& s, const bool incrInserted=true) {
+	candidates_->push(s, incrInserted);
+    }
+    inline void push(const int n, CoinTreeNode** nodes,
+		     const bool incrInserted = true) {
+	candidates_->push(n, nodes, incrInserted);
     }
 
     inline CoinTreeNode* bestQualityCandidate() const {
