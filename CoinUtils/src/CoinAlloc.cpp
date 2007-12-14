b2b0d671149aa6b09e994227ed77c69bd75016b3
Laci Ladanyi <ladanyi@us.ibm.com>
2007-12-14 20:50:12 +0000

    Added empty files for CoinAlloc

---
+++ CoinUtils/src/CoinAlloc.cpp
@@ -0,0 +1,5 @@
+// Copyright (C) 2000, International Business Machines
+// Corporation and others.  All Rights Reserved.
+
+#include "CoinAlloc.hpp"
+
---
+++ CoinUtils/src/CoinAlloc.hpp
@@ -0,0 +1,7 @@
+// Copyright (C) 2000, International Business Machines
+// Corporation and others.  All Rights Reserved.
+
+#ifndef CoinAlloc_H
+#define CoinAlloc_H
+
+#endif
