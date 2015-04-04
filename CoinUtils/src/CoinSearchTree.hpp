763d2ec6315d845bd5542f16a5be9128c9799b79
Ted Ralphs <ted@lehigh.edu>
2015-04-04 16:25:34 +0000

    Small bug fix for search tree

--- CoinUtils/src/CoinSearchTree.hpp
+++ CoinUtils/src/CoinSearchTree.hpp
@@ -243,7 +243,7 @@ public:
     inline int size() const { return size_; }
     inline int numInserted() const { return numInserted_; }
     inline CoinTreeNode* top() const {
-      if (size_ == 0)
+       if (size_ == 0 || candidateList_.size() == 0)
 	return NULL;
 #ifdef DEBUG_PRINT
       char output[44];
