e73c99c341fa26c3a65e2c847bf1e6c22754d331
Stefan Vigerske <svigerske@gams.com>
2020-01-09 09:21:16 +0000

    export more functions
    - used in Cgl

--- CoinUtils/src/clique_separation.h
+++ CoinUtils/src/clique_separation.h
@@ -9,21 +9,29 @@ typedef struct _CliqueSeparation CliqueSeparation;
 /* origGraph is the original conflict graph,
    containing all nodes. separation will always
    process a subset of these nodes */
+COINUTILSLIB_EXPORT
 CliqueSeparation *clq_sep_create(const CGraph *origGraph);
 
 /* separates clique inequalities for fractional solution x */
+COINUTILSLIB_EXPORT
 bool clq_sep_separate(CliqueSeparation *sep, const double x[]);
 
 /* returns separated clique inequalities */
+COINUTILSLIB_EXPORT
 const CliqueSet *clq_sep_get_cliques(CliqueSeparation *sep);
 
+COINUTILSLIB_EXPORT
 void clq_sep_free(CliqueSeparation **clqSep);
 
 /* if clique will be extended using reduced costs, an array for
    these values should be informed before each clq_sep_separate call */
+COINUTILSLIB_EXPORT
 void clq_sep_set_rc(CliqueSeparation *sep, const double rc[]);
+COINUTILSLIB_EXPORT
 void clq_sep_set_extend_method(CliqueSeparation *sep, const size_t extendC);
+COINUTILSLIB_EXPORT
 void clq_sep_set_max_it_bk(CliqueSeparation *clqSep, size_t maxItBK);
+COINUTILSLIB_EXPORT
 void clq_sep_set_max_it_bk_ext(CliqueSeparation *clqSep, size_t maxItBK);
 
 #endif
