d1083088f60a8e257d8c58ef0ad65b80e5be1410
André Apitzsch <andre.apitzsch@etit.tu-chemnitz.de>
2024-07-19 15:07:32 +0200

    CoinFinite: Remove unneeded includes

--- src/CoinFinite.cpp
+++ src/CoinFinite.cpp
@@ -3,18 +3,8 @@
 // This code is licensed under the terms of the Eclipse Public License (EPL).
 
 #include "CoinFinite.hpp"
-#include "CoinUtilsConfig.h"
 
-#include <cfloat>
 #include <cmath>
-
-#ifdef HAVE_CIEEEFP
-#include <cieeefp>
-#else
-#ifdef HAVE_IEEEFP_H
-#include <ieeefp.h>
-#endif
-#endif
 
 bool CoinFinite(double val)
 {
