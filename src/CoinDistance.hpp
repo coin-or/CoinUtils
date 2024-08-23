f1264b49e86bd3f5b69c6815bc398cefa07ed508
André Apitzsch <andre.apitzsch@etit.tu-chemnitz.de>
2024-08-23 18:37:20 +0200

    Remove SUNPRO switch from coinDistance()
    The Sun/Oracle compiler should follow the standard by now.

--- src/CoinDistance.hpp
+++ src/CoinDistance.hpp
@@ -23,25 +23,13 @@ template < class ForwardIterator, class Distance >
 void coinDistance(ForwardIterator first, ForwardIterator last,
   Distance &n)
 {
-#if defined(__SUNPRO_CC)
-  n = 0;
-  std::distance(first, last, n);
-#else
   n = std::distance(first, last);
-#endif
 }
 
 template < class ForwardIterator >
 size_t coinDistance(ForwardIterator first, ForwardIterator last)
 {
-  size_t retVal;
-#if defined(__SUNPRO_CC)
-  retVal = 0;
-  std::distance(first, last, retVal);
-#else
-  retVal = std::distance(first, last);
-#endif
-  return retVal;
+  return std::distance(first, last);
 }
 
 #endif
