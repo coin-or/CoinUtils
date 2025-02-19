2a7c212fca4f71c5f590d67e236f6491a7fa3834
Jan-Willem Goossens <jhmgoossens@hotmail.com>
2025-02-19 21:53:26 +0100

    Add output for CoinRationalTest

--- test/CoinRationalTest.cpp
+++ test/CoinRationalTest.cpp
@@ -13,6 +13,7 @@
 #endif
 
 #include <cassert>
+#include <iostream>
 #include "CoinRational.hpp"
 
 void
@@ -21,15 +22,17 @@ CoinRationalUnitTest()
 
     // Test default constructor
     {
-         CoinRational r; // 0/1
-        assert(r.getNumerator() == 0);
-        assert(r.getDenominator() == 1);
+        CoinRational a; // 0/1
+        std::cout << "Testing " << a.getNumerator() << " / " << a.getDenominator() << std::endl;
+        assert(a.getNumerator() == 0);
+        assert(a.getDenominator() == 1);
     }
 
     // Requires int64_t
     // Test constructor with assignment
     {
         CoinRational a(4294967295, 4294967296);
+        std::cout << "Testing " << a.getNumerator() << " / " << a.getDenominator() << std::endl;
         assert(a.getNumerator() == 4294967295);
         assert(a.getDenominator() == 4294967296);
     }
@@ -38,6 +41,7 @@ CoinRationalUnitTest()
     // Test constructor with assignment
     {
         CoinRational a(9223372036854775807, 4294967299);
+        std::cout << "Testing " << a.getNumerator() << " / " << a.getDenominator() << std::endl;
         assert(a.getNumerator() == 9223372036854775807);
         assert(a.getDenominator() == 4294967299);
     }
@@ -46,19 +50,22 @@ CoinRationalUnitTest()
     // Test constructor with nearestRational calls
     {
         CoinRational a(2147483699.5, 0.00001, 4294967299);
-        assert(a.getNumerator() == 4294967299);
+        std::cout << "Testing " << a.getNumerator() << " / " << a.getDenominator() << std::endl;
+        assert(a.getNumerator() == 4294967399);
         assert(a.getDenominator() == 2);
     }
 
     // Test constructor with nearestRational calls
     {
         CoinRational a(-3.0, 0.0001, 100);
+        std::cout << "Testing " << a.getNumerator() << " / " << a.getDenominator() << std::endl;
         assert(a.getNumerator() == -3);
         assert(a.getDenominator() == 1);
     }
 
     {
         CoinRational a(3.0, 0.0001, 100);
+        std::cout << "Testing " << a.getNumerator() << " / " << a.getDenominator() << std::endl;
         assert(a.getNumerator() == 3);
         assert(a.getDenominator() == 1);
     }
@@ -66,12 +73,14 @@ CoinRationalUnitTest()
     {
         // return 0/1 if best is more than maxdelta tolerance
         CoinRational a(0.367879441, 0.0001, 100); // 1/e
+        std::cout << "Testing " << a.getNumerator() << " / " << a.getDenominator() << std::endl;
         assert(a.getNumerator() == 32);
         assert(a.getDenominator() == 87);
     }
 
     {
         CoinRational a(10.367879441, 0.0001, 100); // 10 + 1/e
+        std::cout << "Testing " << a.getNumerator() << " / " << a.getDenominator() << std::endl;
         assert(a.getNumerator() == 902);
         assert(a.getDenominator() == 87);
     }
@@ -79,18 +88,21 @@ CoinRationalUnitTest()
     {
         // return 0/1 if best is more than maxdelta tolerance
         CoinRational a(0.367879441, 0.000001, 100); // 1/e
+        std::cout << "Testing " << a.getNumerator() << " / " << a.getDenominator() << std::endl;
         assert(a.getNumerator() == 0);
         assert(a.getDenominator() == 1);
     }
 
     {
         CoinRational a(3.0 / 7.0, 0.0001, 100);
+        std::cout << "Testing " << a.getNumerator() << " / " << a.getDenominator() << std::endl;
         assert(a.getNumerator() == 3);
         assert(a.getDenominator() == 7);
     }
 
     {
         CoinRational a(7.0 / 3.0, 0.0001, 100);
+        std::cout << "Testing " << a.getNumerator() << " / " << a.getDenominator() << std::endl;
         assert(a.getNumerator() == 7);
         assert(a.getDenominator() == 3);
     }
@@ -98,6 +110,7 @@ CoinRationalUnitTest()
     {
         double sqrt13 = sqrt(13);
         CoinRational a(sqrt13, 0.01, 20);
+        std::cout << "Testing " << a.getNumerator() << " / " << a.getDenominator() << std::endl;
         assert(a.getNumerator() == 18);
         assert(a.getDenominator() == 5);
     }
@@ -105,42 +118,49 @@ CoinRationalUnitTest()
     {
         double sqrt13 = sqrt(13);
         CoinRational a(sqrt13, 0.002, 30);
+        std::cout << "Testing " << a.getNumerator() << " / " << a.getDenominator() << std::endl;
         assert(a.getNumerator() == 101);
         assert(a.getDenominator() == 28);
     }
 
     {
         CoinRational a(0.25, 0.1, 3);
+        std::cout << "Testing " << a.getNumerator() << " / " << a.getDenominator() << std::endl;
         assert(a.getNumerator() == 1);
         assert(a.getDenominator() == 3);
     }
 
     {
         CoinRational a(0.605551, 0.003, 30);
+        std::cout << "Testing " << a.getNumerator() << " / " << a.getDenominator() << std::endl;
         assert(a.getNumerator() == 17);
         assert(a.getDenominator() == 28);
     }
         
     {
         CoinRational a(0.605551, 0.001, 30);
+        std::cout << "Testing " << a.getNumerator() << " / " << a.getDenominator() << std::endl;
         assert(a.getNumerator() == 20);
         assert(a.getDenominator() == 33); // oops, should be at most 30.
     }
 
     {
         CoinRational a(0.58496250072, 0.00001, 253);
+        std::cout << "Testing " << a.getNumerator() << " / " << a.getDenominator() << std::endl;
         assert(a.getNumerator() == 179);
         assert(a.getDenominator() == 306);  // oops, should be at most 253. Expected 148/253, but this is apparently on purpose.
     }
 
     {
         CoinRational a(19.0/11.0, 0.02, 10);
+        std::cout << "Testing " << a.getNumerator() << " / " << a.getDenominator() << std::endl;
         assert(a.getNumerator() == 12);
         assert(a.getDenominator() == 7);
     }
 
     {
         CoinRational a(-19.0 / 11.0, 0.02, 10);
+        std::cout << "Testing " << a.getNumerator() << " / " << a.getDenominator() << std::endl;
         assert(a.getNumerator() == -12);
         assert(a.getDenominator() == 7);
     }
